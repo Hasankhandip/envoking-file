53629fe45533a48fd134538cb2497b62300d7a09
Dip <hasankhandip@gmail.com>
2022-12-08 01:53:54 +0600

    add banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,27 @@ $(".up-arrow").on("click", function () {
 });
 // scroll-top btn end
 
+// btn js start
+$( ".button_su_inner" ).mouseenter(function(e) {
+  var parentOffset = $(this).offset(); 
+ 
+  var relX = e.pageX - parentOffset.left;
+  var relY = e.pageY - parentOffset.top;
+  $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
+  $(this).prev(".su_button_circle").removeClass("desplode-circle");
+  $(this).prev(".su_button_circle").addClass("explode-circle");
+
+});
+
+$( ".button_su_inner" ).mouseleave(function(e) {
+
+    var parentOffset = $(this).offset(); 
+
+    var relX = e.pageX - parentOffset.left;
+    var relY = e.pageY - parentOffset.top;
+    $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
+    $(this).prev(".su_button_circle").removeClass("explode-circle");
+    $(this).prev(".su_button_circle").addClass("desplode-circle");
+
+});
+// btn js end
