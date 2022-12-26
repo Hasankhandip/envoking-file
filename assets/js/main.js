9b96446d192658af70680c6dd50fbc8a761348c5
Dip <hasankhandip@gmail.com>
2022-12-27 01:36:34 +0600

    service-page added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,20 @@ $(".top-btn").on("click", function () {
   $("html, body").animate({ scrollTop: 0 }, 500);
 });
 // scroll-top btn end
+// slick slider start
+$(document).ready(function(){
+  $('.testimonial-area').slick({
+    autoplay:true,
+    prevArrow: ".left-arrow-testimonial",
+    nextArrow:  ".right-arrow-testimonial",
+    slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        dots:true,
+      }
+    }
+  ]
+  });});
